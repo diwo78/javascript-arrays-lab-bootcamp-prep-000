1a5c5c461646f30746bfa8d8d5f3204823f27108
Diego Z <diego.zegarra78@gmail.com>
2018-08-10 19:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ var kittens = ["Milo","Otis","Garfield"];
 function destructivelyAppendKitten(Ralph){
    kittens.push(Ralph);
    return kittens
-   
+}
+function destructivelyPrependKitten(Bob){
+  kittens.unshift(Bob);
+  return kittens
 }
